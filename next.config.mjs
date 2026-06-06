e2a203b3d60eb3738c3332a8c8eb63f6d958382a
Shakibul-islam-Dev <ishakibul98@gmail.com>
2026-06-06 20:07:40 +0600

    solving problem 2nd time 2nd

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  env: {
+    BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
+    BETTER_AUTH_URL: process.env.BETTER_AUTH_URL,
+    MONGO_DB_URI: process.env.MONGO_DB_URI,
+  },
 };
 
 export default nextConfig;
